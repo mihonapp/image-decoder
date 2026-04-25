e03b81e18a6b5f27e094ca48f548afa173baa9d0
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2026-04-25 06:57:05 +0600

    Modernize build configuration (#5)

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ImageDecoder"
+
+include(":library")
